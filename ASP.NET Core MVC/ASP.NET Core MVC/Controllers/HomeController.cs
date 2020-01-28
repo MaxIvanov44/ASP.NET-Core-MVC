5b99786b5f343b290e426ae9a24723dd79420d6b
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-01-28 20:45:22 +0300

    AdditionOfTheBuying

--- ASP.NET Core MVC/ASP.NET Core MVC/Controllers/HomeController.cs
+++ ASP.NET Core MVC/ASP.NET Core MVC/Controllers/HomeController.cs
@@ -20,5 +20,23 @@ namespace ASP.NET_Core_MVC.Controllers
         {
             return View(db.Phones.ToList());
         }
+
+
+
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.PhoneId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
     }
 }
